050bb18e364363d0af0b6aa0d21722a8c6a0f8ae
Furkan Samaraz <92402372+FurkanSamaraz@users.noreply.github.com>
2022-03-04 00:19:53 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	router = gin.Default()
+)
+
+func main() {
+	router.POST("/login", Login)
+	router.GET("/login", Loginget)
+	log.Fatal(router.Run(":8080"))
+}
+
+type User struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Phone    string `json:"phone"`
+}
+
+var user = User{
+	ID:       1,
+	Username: "Furkan",
+	Password: "Samaraz",
+	Phone:    "202007105034", //this is a random number
+}
+
+func Loginget(c *gin.Context) {
+	c.JSON(http.StatusOK, " NO TOKENS ")
+}
+func Login(c *gin.Context) {
+	var u User
+
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Geçersiz json")
+		return
+	}
+
+	//compare the user from the request, with the one we defined:
+	if user.Username != u.Username || user.Password != u.Password {
+		c.JSON(http.StatusUnauthorized, "Lütfen geçerli giriş bilgilerini sağlayın")
+		return
+	}
+	token, err := CreateToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, token)
+}
+func CreateToken(userId uint64) (string, error) {
+	var err error
+	//Creating Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userId
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
